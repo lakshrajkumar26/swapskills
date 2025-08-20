6826766571bef028b7ea3fb7c4949fa665f9aea7
lakshrajkumar26 <110077095+lakshrajkumar26@users.noreply.github.com>
2025-08-20 23:00:20 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -49,7 +49,7 @@ const server = http.createServer(app);
 // Attach socket.io
 const io = socketio(server,{
     cors:{
-        origin: "http://localhost:5173",
+        origin: allowedOrigin,
         methods :["GET","POST","PUT"]
     }
 });
